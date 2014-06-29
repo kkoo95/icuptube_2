57da3fc0e5ba71de3ab6baa3c4d40ffcf60b2c20
jojo <kkoo95@gmail.com>
2014-06-29 09:13:23 +0000

    test quick update on heroku source

--- js/app.js
+++ js/app.js
@@ -1,14 +1,71 @@
 App = Ember.Application.create();
 
-App.ApplicationManager = DS.FixtureAdapter.extend({
-});
-
 App.Router.map(function() {
-  // put your routes here
 });
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
-    return ['red', 'yellow', 'blue'];
+    return this.store.findAll('video');
   }
 });
+/*
+App.VideoboxComponent = Ember.Component.extend({
+  template: function() {
+    return Ember.Handlebars.compile("<div>youtube.api" + video.get('youtubeid') + "</div>");
+  }.property()
+});
+*/
+
+App.ApplicationAdapter = DS.FixtureAdapter.extend({
+});
+
+App.Video = DS.Model.extend({
+  youtubeId : DS.attr('string')
+});
+
+App.Video.FIXTURES = [
+  {
+    id: 100,
+    youtubeid: "0PiwXwy3iWg"
+  },
+  {
+    id: 101,
+    youtubeid: "ESXgJ9-H-2U"
+  },
+  {
+    id: 102,
+    youtubeid: "foXJAV4bRiE"
+  },
+  {
+    id: 103,
+    youtubeid: "qthZfLY74Lw"
+  },
+  {
+    id: 104,
+    youtubeid: "NazVKnD-_sQ"
+  },
+  {
+    id: 105,
+    youtubeid: "zOfRtSW4dYE"
+  },
+  {
+    id: 106,
+    youtubeid: "MpHAp7zG7tI"
+  },
+  {
+    id: 107,
+    youtubeid: "UIZnpUS62Uk"
+  },
+  {
+    id: 108,
+    youtubeid: "7X5b76mdD84"
+  },
+  {
+    id: 109,
+    youtubeid: "lpWMCulJrOc"
+  },
+  {
+    id: 110,
+    youtubeid: "7b3cvgAvC4o"
+  }
+]
