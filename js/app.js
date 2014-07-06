9614cca8ed673be905776ae37690879c2afa9f98
jojo <kkoo95@gmail.com>
2014-07-06 23:26:18 +0000

    add feature in

--- js/app.js
+++ js/app.js
@@ -4,9 +4,28 @@ var App = Ember.Application.create({
   }
 });
 
+// wait for yt video images to be loaded
 App.deferReadiness();
 
 App.Router.map(function() {
+});
+
+App.ApplicationController = Ember.Controller.extend({
+  videoId: "",
+  actions: {
+    addVideo: function() {
+      var video = this.store.createRecord('video', { 
+        youtubeid: this.get('videoId')
+      }); 
+      
+      var controller = this; 
+
+      video.save().then(function(video) { 
+        controller.set('videoId', '');
+        controller.controllerFor('index').get('model').addObject(video); 
+      });
+    }
+  }
 });
 
 App.IndexRoute = Ember.Route.extend({
@@ -15,12 +34,18 @@ App.IndexRoute = Ember.Route.extend({
   }
 });
 
+App.IndexController = Ember.ArrayController.extend({
+  sortProperties: ['id'],
+  sortAscending: false,
+});
+
 App.VideoBoxComponent = Ember.Component.extend({
+  ytVideoTitle: null,
   ytVideoImgSrc: null,
   
   ytVideoImg: function() {
     if (this.get('ytVideoImgSrc')) {
-      return new Handlebars.SafeString("<img class='ytimg' height='200' width='200' src='" + this.get('ytVideoImgSrc') + "'/>");
+      return new Handlebars.SafeString("<img class='ytimg' src='" + this.get('ytVideoImgSrc') + "' title='"+ this.get('ytVideoTitle') +"'/>");
     }
     else
       return new Handlebars.SafeString("<img />");
@@ -29,21 +54,81 @@ App.VideoBoxComponent = Ember.Component.extend({
   loadImg: function() {
     var view = this;
 
-    getYTVideoInfo(this.get('video.youtubeid'), function(response) {
+    fetchYTVideoInfo(this.get('video.youtubeid'), function(response) {
       var info = response.items[0];
-      view.set('ytVideoImgSrc', info.snippet.thumbnails.medium.url);  
+      view.set('ytVideoImgSrc', info.snippet.thumbnails.medium.url); 
+      view.set('ytVideoTitle', info.snippet.title);
     });
   }.on('init'),
   
   click: function(event) {
+    if (playingComponent == this)
+      return;
+    
     if (player) {
+      targetComponent = this;
+      switchingVideo = true;
       player.stopVideo();
     }
+    else {
+      this.playVideo();
+    }
+  },
+  
+  playVideo: function() {
+      if (player)
+          player.removeEventListener("onStateChange", "onPlayerStateChange");
+
+      player = newYTPlayer('#' + this.elementId + " .ytimg", this.get('video.youtubeid'));
     
-    component = this;
-    player = newYTPlayer(this.elementId, this.get('video.youtubeid'));
+      player.addEventListener("onStateChange", "onPlayerStateChange");
+    
+      playingComponent = this;
   }
 });
+
+var switchingVideo;
+var targetComponent;
+var playingComponent;
+var player;
+
+function onPlayerStateChange(state) {
+  if (switchingVideo)
+    switchPlayer();
+}
+
+function switchPlayer() {
+  if (player && targetComponent) {
+    var stoppingComponentId = $(player.getIframe()).parent().attr('id');
+    var stoppingComponent = Ember.View.views[stoppingComponentId];
+    
+    stoppingComponent.rerender(); 
+    targetComponent.playVideo();
+
+    targetComponent = null;
+    switchingVideo = false;
+  }
+}
+
+function newYTPlayer(selector, id) {
+  return new YT.Player(Ember.$(selector)[0], {
+    videoId: id,
+    playerVars: {
+      autoplay: '1',
+      controls: '0',
+      modestbranding: '1',
+      rel: '0',
+      showinfo: '0',
+      iv_load_policy: '3'
+    }
+  });
+}
+
+function replaceElement(selector, html) {
+  $(selector).replaceWith(function() {
+    return $(html).append($(this).contents());
+  })
+};
 
 App.ApplicationAdapter = DS.FixtureAdapter.extend({
 });
@@ -102,26 +187,7 @@ App.Video.FIXTURES = [
 App.Videobox = DS.Model.extend({
 });
 
-var player;
-var component;
-
-function newYTPlayer(selector, id) {
-  return new YT.Player(selector, {
-    height: '200',
-    width: '200',
-    videoId: id,
-    playerVars: {
-      autoplay: '1',
-      controls: '0',
-      modestbranding: '1',
-      rel: '0',
-      showinfo: '0',
-      iv_load_policy: '3'
-    }
-  });
-}
-
-function getYTVideoInfo(id, callback) {
+function fetchYTVideoInfo(id, callback) {
   var request = gapi.client.youtube.videos.list({
     id: id,
     part: 'snippet',
